ebea760956eff56a33688163304eeaa092d91f5a
Alex Kuznecov <kuzniecov@mail.ru>
2017-02-25 01:22:27 +0300

    init

---
+++ src/main/java/my/home/util/Util.java
@@ -0,0 +1,14 @@
+package my.home.util;
+
+/**
+ * Created by alek on 25.2.17.
+ */
+public class Util {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+
+    }
+
+}
